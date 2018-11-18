0694442b469f316b18387147708151748ff03f3c
Ashot Pahlevanyan <ashotpahlevanyan@gmail.com>
2018-11-18 10:28:22 +0400

    added submit form for url

--- app/renderer.js
+++ app/renderer.js
@@ -1,4 +1,14 @@
-const button = document.querySelector('.alert');
-button.addEventListener('click', () => {
-	alert(__dirname);
+const linksSection = document.querySelector('.links');
+const errorMessage = document.querySelector('.error-message');
+const newLinkForm = document.querySelector('.new-link-form');
+const newLinkUrl = document.querySelector('.new-link-url');
+const newLinkSubmit = document.querySelector('.new-link-submit');
+const clearStorageButton = document.querySelector('.clear-storage');
+
+newLinkUrl.addEventListener('keyup', () => {
+	newLinkSubmit.disabled = !newLinkSubmit.validity.valid;
 });
+
+const clearForm = () => {
+	newLinkUrl.value = null;
+};
