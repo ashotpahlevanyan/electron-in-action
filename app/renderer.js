c474f58e81507883631680da574ececb7c076174
Ashot Pahlevanyan <ashotpahlevanyan@gmail.com>
2018-11-20 12:35:18 +0400

    added title parser to renderer

--- app/renderer.js
+++ app/renderer.js
@@ -1,3 +1,5 @@
+const parser = new DOMParser();
+
 const linksSection = document.querySelector('.links');
 const errorMessage = document.querySelector('.error-message');
 const newLinkForm = document.querySelector('.new-link-form');
@@ -5,9 +7,30 @@ const newLinkUrl = document.querySelector('.new-link-url');
 const newLinkSubmit = document.querySelector('.new-link-submit');
 const clearStorageButton = document.querySelector('.clear-storage');
 
+
+
 newLinkUrl.addEventListener('keyup', () => {
 	newLinkSubmit.disabled = !newLinkSubmit.validity.valid;
 });
+
+newLinkForm.addEventListener('submit', () => {
+	event.preventDefault();
+
+	const url = newLinkUrl.value;
+	fetch(url)
+		.then(response => response.text())
+		.then(parseResponse)
+		.then(findTitle)
+		.then(alert);
+});
+
+const parseResponse = (text) => {
+	return parser.parseFromString(text, 'text/html');
+};
+
+const findTitle = (nodes) => {
+	return nodes.querySelector('title').innerText;
+};
 
 const clearForm = () => {
 	newLinkUrl.value = null;
